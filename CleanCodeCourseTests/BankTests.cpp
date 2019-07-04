54c85148fa63ab677c25f80e76201e6c4b4812f4
rdrangova <rdrungova@gmail.com>
2019-07-05 02:06:07 +0300

    tests commit

---
+++ CleanCodeCourseTests/BankTests.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../CleanCodeCourse2019/Bank.h"
+#include "../CleanCodeCourse2019/Bank.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BankTests
+{
+	TEST_CLASS(BankTests)
+	{
+	public:
+
+		TEST_METHOD(testAddCustomer)
+		{
+			Bank Radus("Radus International Bank", "Dondukov 1, Sofia");
+			unsigned long long id = 666;
+			Assert::AreEqual(id,Radus.getCustomers()[0].getId());
+		}
+
+		TEST_METHOD(testDeleteCustomer)
+		{
+			Bank Radus("Radus International Bank", "Dondukov 1, Sofia");
+			unsigned long long id = 666;
+			string name = "Radost";
+			string address = "Studentski grad";
+			Radus.addCustomer(id, name, address);
+
+			Radus.deleteCustomer(id);
+			Assert::AreEqual(0, int(Radus.getCustomers().size()));
+		}
+
+
+	};
+}
